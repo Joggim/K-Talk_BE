2c0ed04f8281f13cca62457a55bbe495161b07f6
haeun228 <123haeun@naver.com>
2025-02-13 19:54:55 +0900

    Feat: ApiResponse 구현

---
+++ src/main/java/com/joggim/k_talk/common/ApiResponse.java
@@ -0,0 +1,24 @@
+package com.joggim.k_talk.common;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ApiResponse<T> {
+    private boolean success;
+    private String message;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private T data;
+
+    public static <T> ApiResponse<T> success(String message, T data) {
+        return new ApiResponse<>(true, message, data);
+    }
+
+    public static <T> ApiResponse<T> fail(String message) {
+        return new ApiResponse<>(false, message, null);
+    }
+}
