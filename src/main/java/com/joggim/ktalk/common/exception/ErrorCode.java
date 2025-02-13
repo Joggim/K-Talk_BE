7e91350e3d3461c55319c13f71978384fb1d0f0e
haeun228 <123haeun@naver.com>
2025-02-13 20:30:59 +0900

    Feat: 공통 예외 처리 & 에러 코드 추가

---
+++ src/main/java/com/joggim/ktalk/common/exception/ErrorCode.java
@@ -0,0 +1,16 @@
+package com.joggim.ktalk.common.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다."),
+    INVALID_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 오류가 발생했습니다.");
+
+    private final HttpStatus status;
+    private final String message;
+}
