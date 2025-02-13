eafa6189a9408a46f76b9362b9de6f35d5da6690
haeun228 <123haeun@naver.com>
2025-02-13 19:10:02 +0900

    Init: 프로젝트 초기 세팅

---
+++ src/main/java/com/joggim/k_talk/KTalkApplication.java
@@ -0,0 +1,13 @@
+package com.joggim.k_talk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KTalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KTalkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joggim/k_talk/KTalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joggim.k_talk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KTalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
