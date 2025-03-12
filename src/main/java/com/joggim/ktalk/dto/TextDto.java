7218fe12e2da61480d4a4b0d67f7be9b234497f6
haeun228 <123haeun@naver.com>
2025-03-12 15:53:08 +0900

    Feat: TextDto 생성

---
+++ src/main/java/com/joggim/ktalk/dto/TextDto.java
@@ -0,0 +1,16 @@
+package com.joggim.ktalk.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TextDto {
+
+    private String text;
+
+}
