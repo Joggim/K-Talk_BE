1487d68093bedd6a1822a56a9edeac253431c715
haeun228 <123haeun@naver.com>
2025-03-14 15:54:47 +0900

    Feat: TopicRepository & SentenceRepository 구현

---
+++ src/main/java/com/joggim/ktalk/repository/SentenceRepository.java
@@ -0,0 +1,12 @@
+package com.joggim.ktalk.repository;
+
+import com.joggim.ktalk.domain.Sentence;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface SentenceRepository extends JpaRepository<Sentence, Long> {
+    List<Sentence> findByTopicId(Long topicId);
+}
---
+++ src/main/java/com/joggim/ktalk/repository/TopicRepository.java
@@ -0,0 +1,9 @@
+package com.joggim.ktalk.repository;
+
+import com.joggim.ktalk.domain.Topic;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TopicRepository extends JpaRepository<Topic, Long> {
+}
