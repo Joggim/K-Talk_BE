f5214eefd6437d334411df7b56ceb9573e6160a3
haeun228 <123haeun@naver.com>
2025-03-14 15:55:42 +0900

    Feat: TopicService & SentenceService 구현

---
+++ src/main/java/com/joggim/ktalk/service/SentenceService.java
@@ -0,0 +1,33 @@
+package com.joggim.ktalk.service;
+
+import com.joggim.ktalk.common.exception.CustomException;
+import com.joggim.ktalk.common.exception.ErrorCode;
+import com.joggim.ktalk.domain.Sentence;
+import com.joggim.ktalk.dto.SentenceDto;
+import com.joggim.ktalk.repository.SentenceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class SentenceService {
+
+    @Autowired
+    private SentenceRepository sentenceRepository;
+
+    public SentenceDto.SentenceDetail getSentenceById(Long sentenceId) {
+        Sentence sentence = sentenceRepository.findById(sentenceId).orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND));
+
+        return SentenceDto.SentenceDetail.fromEntity(sentence);
+    }
+
+    public List<SentenceDto.SentenceSummary> getSentencesByTopic(Long topicId) {
+        List<Sentence> sentences = sentenceRepository.findByTopicId(topicId);
+
+        return sentences.stream()
+                .map(SentenceDto.SentenceSummary::fromEntity)
+                .collect(Collectors.toList());
+    }
+}
