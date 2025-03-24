a41bc2b7dd49a2841c16d665a7cf007088ef4c31
haeun228 <123haeun@naver.com>
2025-03-24 16:35:21 +0900

    Feat: AudioRequestDto 생성

---
+++ src/main/java/com/joggim/ktalk/dto/AudioRequestDto.java
@@ -0,0 +1,16 @@
+package com.joggim.ktalk.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AudioRequestDto {
+
+    private String audio;
+
+}
