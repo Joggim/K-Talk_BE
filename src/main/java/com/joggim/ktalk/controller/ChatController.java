d9564f741c20533cf37f2066e4af5192b70321f8
haeun228 <123haeun@naver.com>
2025-04-03 16:18:55 +0900

    Feat: 채팅방 생성 API 구현 중

--- src/main/java/com/joggim/ktalk/controller/ChatController.java
+++ src/main/java/com/joggim/ktalk/controller/ChatController.java
@@ -1,16 +1,15 @@
 package com.joggim.ktalk.controller;
 
 import com.joggim.ktalk.common.ApiResponse;
+import com.joggim.ktalk.dto.AudioRequestDto;
+import com.joggim.ktalk.dto.UserMessageDto;
 import com.joggim.ktalk.service.ChatService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,6 +23,15 @@ public class ChatController {
     // 대화 목록 조회
 
     // 대화 시작
+    @PostMapping
+    public ResponseEntity<ApiResponse<UserMessageDto>> createChatRoom(AudioRequestDto dto, @AuthenticationPrincipal User user) {
+        String userId = user.getUsername();
+        UserMessageDto message = chatService.createChatRoom(dto, userId);
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(ApiResponse.success("새로운 채팅 생성 성공!", message));
+    }
 
     // 채팅방 조회
     @GetMapping("/{chatRoomId}")
