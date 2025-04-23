ec6b72ac8c47fde5177a56963f6976d8f5ede3f2
haeun228 <123haeun@naver.com>
2025-04-23 13:14:31 +0900

    Feat: 채팅방 조회 API 수정

--- src/main/java/com/joggim/ktalk/controller/ChatController.java
+++ src/main/java/com/joggim/ktalk/controller/ChatController.java
@@ -1,8 +1,6 @@
 package com.joggim.ktalk.controller;
 
 import com.joggim.ktalk.common.ApiResponse;
-import com.joggim.ktalk.dto.AudioRequestDto;
-import com.joggim.ktalk.dto.UserMessageDto;
 import com.joggim.ktalk.service.ChatService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -20,27 +18,27 @@ public class ChatController {
     @Autowired
     private ChatService chatService;
 
-    // 대화 목록 조회
-
-    // 대화 시작
-    @PostMapping
-    public ResponseEntity<ApiResponse<UserMessageDto>> createChatRoom(AudioRequestDto dto, @AuthenticationPrincipal User user) {
-        String userId = user.getUsername();
-        UserMessageDto message = chatService.createChatRoom(dto, userId);
-
-        return ResponseEntity
-                .status(HttpStatus.OK)
-                .body(ApiResponse.success("새로운 채팅 생성 성공!", message));
-    }
-
     // 채팅방 조회
-    @GetMapping("/{chatRoomId}")
-    public ResponseEntity<ApiResponse<List<Object>>> getMessages(@PathVariable Long chatRoomId) {
-        List<Object> messages = chatService.getMessages(chatRoomId);
+    @GetMapping("/messages")
+    public ResponseEntity<ApiResponse<List<Object>>> getMessages(@AuthenticationPrincipal User principal) {
+        String userId = principal.getUsername();
+        List<Object> messages = chatService.getMessages(userId);
 
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(ApiResponse.success("채팅방 조회 성공!", messages));
     }
+
+
+//    // 대화 시작
+//    @PostMapping
+//    public ResponseEntity<ApiResponse<UserMessageDto>> createChatRoom(AudioRequestDto dto, @AuthenticationPrincipal User user) {
+//        String userId = user.getUsername();
+//        UserMessageDto message = chatService.createChatRoom(dto, userId);
+//
+//        return ResponseEntity
+//                .status(HttpStatus.OK)
+//                .body(ApiResponse.success("새로운 채팅 생성 성공!", message));
+//    }
 
 }
