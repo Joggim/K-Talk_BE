b7f5d37e1ba632c79f6ac9571bfe5b4e82b57870
haeun228 <123haeun@naver.com>
2025-04-30 17:45:07 +0900

    Feat: 맞춤 학습 추천을 위한 Sentence 엔티티 수정 & 관련 엔티티 추가

---
+++ src/main/java/com/joggim/ktalk/domain/PronunciationIssue.java
@@ -0,0 +1,27 @@
+package com.joggim.ktalk.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PronunciationIssue {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title; // 예: "된소리 구분", "유성음화"
+
+    @OneToMany(mappedBy = "issue", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Sentence> sentences = new ArrayList<>();
+
+}
