2c95dc69629ebec07657719f27779336979714d2
haeun228 <123haeun@naver.com>
2025-05-02 09:49:06 +0900

    Fix: 사용자 맞춤 학습 추천 조회 API 경로 수정

--- src/main/java/com/joggim/ktalk/controller/LearningRecommendationController.java
+++ src/main/java/com/joggim/ktalk/controller/LearningRecommendationController.java
@@ -9,11 +9,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/learning")
 public class LearningRecommendationController {
 
     @Autowired
